87a97f9ff9fa06f6f48ed9617e3a0de37fc70523
Luck4eR <kfrxth@gmail.com>
2022-10-23 20:35:01 +0300

    fixed 1.2 + 3.3

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -1,9 +1,25 @@
-/**
- * trimSymbols - removes consecutive identical symbols if they quantity bigger that size
- * @param {string} string - the initial string
- * @param {number} size - the allowed size of consecutive identical symbols
- * @returns {string} - the new string without extra symbols according passed size
- */
 export function trimSymbols(string, size) {
+  let countSameWord = 0;
+  let result = "";
 
+  if (string.length == 0 || size == 0) {
+    return "";
+  }
+
+  if (typeof size == "undefined") {
+    return string;
+  }
+
+  for (let i = 0; i < string.length; i++) {
+    if (countSameWord < size) {
+      result += string[i];
+      countSameWord++;
+    }
+
+	if (string[i + 1] !== string[i]){
+		countSameWord = 0;
+	}
+  }
+
+  return result;
 }
