61c27c280811927294101a1215b45215b1e96afd
Luck4eR <kfrxth@gmail.com>
2022-10-25 22:50:54 +0300

    fixed 2.3

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -1,11 +1,10 @@
 export const omit = (obj, ...fields) => {
   return Object.entries(obj)
     .filter((item) => {
-      return ![...fields].includes(item[0]);
+      return !fields.includes(item[0]);
     })
 
     .reduce((result, cur) => {
-      result[cur[0]] = cur[cur[0]];
       result[cur[0]] = cur[1];
       return result;
     }, {});
