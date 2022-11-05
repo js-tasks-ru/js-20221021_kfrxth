00a1fedba3d81632f4b2837e658f79b87c392ef3
Luck4eR <kfrxth@gmail.com>
2022-11-05 20:01:12 +0300

    try 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,21 @@
 export default class NotificationMessage {
+	element;
 
+  constructor(textNotification = "", { duration = 1000, type = "success" }) {
+	this.textNotification = textNotification;
+    this.duration = duration;
+    this.type = type;
+    this.render();
+  }
+
+  render() {}
+
+  destroy() {
+	this.remove();
+    this.element = null;
+  }
+
+  remove() {
+	this.element.remove();
+  }
 }
