d71c92f3787f36fda244528902730dc95a8007ab
Luck4eR <kfrxth@gmail.com>
2022-11-05 20:01:36 +0300

    try 5.1

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,21 +1,54 @@
 export default class NotificationMessage {
-	element;
-
-  constructor(textNotification = "", { duration = 1000, type = "success" }) {
-	this.textNotification = textNotification;
+  constructor(
+    textNotification = "",
+    { duration = 1000, type = "success" } = {}
+  ) {
+    this.textNotification = textNotification;
     this.duration = duration;
+	this.seconds = (duration / 1000) + 's';
     this.type = type;
+
     this.render();
   }
 
-  render() {}
+  render() {
+    const element = document.createElement("div");
 
-  destroy() {
-	this.remove();
-    this.element = null;
+    element.innerHTML = this.template();
+
+	if (!document.querySelector(`.notification`)) {
+		document.body.append(element);
+		setTimeout(() => element.remove(), this.duration);
+	}
+
+    this.element = element.firstElementChild;
+  }
+
+  template() {
+    return `
+	<div class="notification ${this.type}" style="--value:${this.seconds}">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+        ${this.textNotification}
+      </div>
+    </div>
+  </div>`;
+  }
+
+  show() {
+    this.render();
   }
 
   remove() {
-	this.element.remove();
+    if (this.element) {
+		this.element.remove();
+	  }
+  }
+
+  destroy() {
+	this.remove();
+	this.element = null;
   }
 }
