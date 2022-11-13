d3f90a7fed8e1273f3c7ff5f0f3a9c2edda1857f
Luck4eR <kfrxth@gmail.com>
2022-11-13 20:12:58 +0300

    task 6.2

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,70 @@
 class Tooltip {
-  initialize () {
+  static instance;
 
+  onPointerOver = (event) => {
+    const element = event.target.closest("[data-tooltip]");
+
+    if (element) {
+      this.render(element.dataset.tooltip);
+      document.addEventListener("pointermove", this.onPointerMove);
+    }
+  };
+
+  onPointerMove = (event) => {
+    this.moveTooltip(event);
+  };
+
+  onPointerOut = () => {
+    this.remove();
+    document.removeEventListener("pointermove", this.onPointerMove);
+  };
+
+  moveTooltip(event) {
+    let shift = 15;
+    const left = event.clientX + shift;
+    const top = event.clientY + shift;
+
+    this.element.style.left = `${left}px`;
+    this.element.style.top = `${top}px`;
+  }
+
+  constructor() {
+    if (Tooltip.instance) {
+      return Tooltip.instance;
+    }
+
+    Tooltip.instance = this;
+  }
+
+  render(html) {
+    this.element = document.createElement("div");
+    this.element.className = "tooltip";
+    this.element.innerHTML = html;
+
+    document.body.append(this.element);
+  }
+
+  initialize() {
+    this.initEventListeners();
+  }
+
+  initEventListeners() {
+    document.addEventListener("pointerover", this.onPointerOver);
+    document.addEventListener("pointerout", this.onPointerOut);
+  }
+
+  destroy() {
+    document.removeEventListener("pointermove", this.onPointerMove);
+    document.removeEventListener("pointerover", this.onPointerOver);
+    document.removeEventListener("pointerout", this.onPointerOut);
+    this.remove();
+    this.element = null;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
   }
 }
 
